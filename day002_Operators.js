39de9334fce43f787cce42d7337560987d5543c2
JesiahTapia <siahtapia@gmail.com>
2024-05-06 21:12:18 -0400

    day002 Operators

---
+++ day002_Operators.js
@@ -0,0 +1,17 @@
+let mealCost = 100
+let tipPercentage = 15
+let taxPercentage = 8
+
+
+solve = (mealCost, tipPercentage, taxPercentage) => {
+    let tipAmount = mealCost * (tipPercentage / 100)
+    let taxAmount = mealCost * (taxPercentage / 100) 
+    let totalBill = mealCost + tipAmount + taxAmount
+
+     console.log(totalBill);
+    return
+}
+
+solve(mealCost,tipPercentage,taxPercentage);
+
+
